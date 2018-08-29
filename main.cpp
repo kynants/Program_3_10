59df965d1910f53375148f1a3dd0e7152602fc3c
Kynan Stevenson <kynan@Kynans-MacBook-Pro.local>
2018-08-29 19:16:33 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 65;
+
+    // Display the value of the number variable.
+    cout << number << endl;
+
+    // Display the value of the number converted to the char data type.
+    cout << static_cast<char>(number) << endl;
+    return 0;
+}
